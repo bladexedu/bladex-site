c25992c56ef60e603932a5e072d5455b3be406e5
KelvinRein7 <khunthurein10.tgi@gmail.com>
2026-03-31 22:52:27 -0400

    Fix Facebook feed width

--- src/pages/Social.jsx
+++ src/pages/Social.jsx
@@ -132,10 +132,10 @@ export default function Social() {
               </div>
               <div className="rounded-3xl overflow-hidden border border-slate-200 w-full" style={{ minHeight: '500px' }}>
                 <iframe
-                  src={`https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D100064021474119&tabs=timeline&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=920970834041656`}
-                  width="100%"
+                  src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D100064021474119&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=true&appId=920970834041656"
+                  width="500"
                   height="500"
-                  style={{ border: 'none', overflow: 'hidden', display: 'block' }}
+                  style={{ border: 'none', overflow: 'hidden', display: 'block', width: '100%' }}
                   scrolling="no"
                   frameBorder="0"
                   allowFullScreen
