8eb261bcc2efa96d6529881d1d6c485f7d10ebd1
KelvinRein7 <khunthurein10.tgi@gmail.com>
2026-07-18 15:19:47 -0400

    new consultant added

--- src/components/consultants/ConsultantCard.jsx
+++ src/components/consultants/ConsultantCard.jsx
@@ -50,7 +50,7 @@ const LOCATION_MAP = {
   'chaw tynzar maung':       'Thailand',
   'yoon myat tharaphi':      'Australia',
   'yupar win thu':           'New Zealand',
-  'chan thar soe':           'Czech Republic · Thailand · Taiwan',
+  'chan thar soe':           'Czech Republic',
 };
 
 export default function ConsultantCard({ consultant: c, index }) {
